9bb14e3c763d1195ddda329e030150e46c54e91e
OzlFX <39440462+OzlFX@users.noreply.github.com>
2020-10-03 14:19:04 +0100

    Initial Player setup

---
+++ Assets/Scripts/Player/PlayerHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerHandler : MonoBehaviour
+{
+    public float m_Speed = 5.0f;
+
+    private Rigidbody2D m_Rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Verticle"));
+        m_Rigidbody2D.AddForce(movement * m_Speed * Time.deltaTime);
+    }
+}
