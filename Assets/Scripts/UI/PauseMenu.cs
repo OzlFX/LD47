9a60505d548ba787b38dabdbf0229e93d41e6a6d
OzlFX <39440462+OzlFX@users.noreply.github.com>
2020-10-04 17:02:04 +0100

    Initial UI setup

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject m_PauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_PauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Escape"))
+        {
+            if (m_PauseMenu.activeSelf)
+            {
+                m_PauseMenu.SetActive(false);
+            }
+            else
+            {
+                m_PauseMenu.SetActive(true);
+            }
+        }
+    }
+}
