97bf50820d499aa8d01706eab58958645145a1e8
OzlFX <39440462+OzlFX@users.noreply.github.com>
2020-10-05 18:37:41 +0100

    Setting up pause menu functionality

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -3,6 +3,7 @@
 public class PauseMenu : MonoBehaviour
 {
     public GameObject m_PauseMenu;
+    public static bool m_Paused = false;
 
     // Start is called before the first frame update
     void Start()
@@ -13,16 +14,30 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Escape"))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (m_PauseMenu.activeSelf)
+            if (m_Paused)
             {
-                m_PauseMenu.SetActive(false);
+                Resume();
             }
             else
             {
-                m_PauseMenu.SetActive(true);
+                Pause();
             }
         }
+    }
+
+    void Resume()
+    {
+        m_PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        m_Paused = false;
+    }
+
+    void Pause()
+    {
+        m_PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        m_Paused = true;
     }
 }
